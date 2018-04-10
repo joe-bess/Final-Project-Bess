331f6bb0e38c44b0bd602155606d80690c6eaf69
joebess <joe.bess41@gmail.com>
2018-04-09 23:11:00 -0400

    Final Project update
    Everything should be functioning properly now with the metadata being added to the page through the iTunes Search API, and the json object provided by Patrick, and the audio files playing properly by referencing them from Patrick's json object.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 **4/03/18**
 Changed the scope of the project to be an artist profile for an artist I've been producing for. I initially was going to incorporate a database to host the metadata, but quickly realized there wasn't much I really wanted to display and it ended up being easier to just use the iTunes Search API to get the metadata for me. The spectrum analyzers are incorporated through p5.js with an instance for each song. The controls are also displayed through the use of p5.js. Expect an update tomorrow when I implement more user functionality allowing the user to search for more information about the artist, using the iTunes Search API to get the data. I would've already implemented that today, but when trying to set myself up with a local server to host the audio files, everything that could've went wrong did and it led me on what seemed like a never-ending google search to troubleshoot error after error. Figuring out how to set up two separate instances of p5 took a bit more time than I would've liked as well.
+
+**4/09/18**
+This took far longer than anticipated, but I think I've finally got a working Final Project idea. No local server is needed to run it as I'm now using data from Patrick's server that he very generously created for me to use for this project. It may be a bit messy for now, but I've designed it to where you pick an artist and track (currently from a limited selection) and the page will display a header for the chosen artist, a p5.js visualizer for the chosen track, and controls to control the track. All the metadata, and audio files, comes from either the iTunes Search API or Patrick's json object that he provided me with for this project. I know it's not perfect as is and there's a slight issue with the audio loading message that I couldn't quite fix, but it does work for the intended purpose.
+
+##### To-Do list
+- Add more tracks and artists
+- Create a style sheet
+- Create a way to display multiple visualizers for different songs at the same time
+  with their own header and set of controls
+- Alter the p5.js sketches so there's only one that I can attach the correct audio to
+  and call whenever I want to display another song on the page
+- Figure out a way to load the audio files before the sketch is created so you don't
+  have to wait for the audio to load before playing the track
--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
+// Javascript for each visualizer
 let song;
 let fft;
 let playbutton, pausebutton;
 let w;
 let slider_1, slider_2;
 let canvas;
+let data;
 
-let viz = function(a) {
-  a.preload = function() {
-    // let url = 'https://patrickmcneill.com/itp/bess-media/audio.json'
-    // song = a.loadJSON(url)
-
-    song = a.loadSound('/media/LoveForYou.mp3');
+let viz_1 = function(a) {
+  a.preload = function () {
+    let url = 'https://patrickmcneill.com/itp/bess-media/audio.json';
+    data = a.loadJSON(url);
   }
 
   a.setup = function() {
+    song = a.loadSound(data[0].file, a.loaded, a.failed, a.loading);
     canvas = a.createCanvas(576, 192);
     canvas.parent('song_viz');
     a.colorMode(a.HSB);
@@ -49,6 +50,23 @@ let viz = function(a) {
     }
     a.endShape();
   }
+  a.loaded = function() {
+    let div = a.createDiv('Audio Loaded!');
+    setTimeout(function() {
+      div.hide();
+      loadingP.style.display = "none";
+      }, 3000);
+  }
+
+  a.failed = function() {
+    a.createDiv('Error: Audio Failed to Load')
+  }
+
+  a.loading = function(percent) {
+    pct = Math.round((percent * 100).toFixed(1) / 10) * 10 + "%";
+    loadingP.style.display = "block";
+    document.getElementById("loading").innerHTML += pct;
+  }
 
   a.playsound = function() {
     if (song.isPlaying() == false) {
@@ -62,59 +80,149 @@ let viz = function(a) {
     }
   }
 };
+let viz_2 = function(a) {
+  a.preload = function () {
+    let url = 'https://patrickmcneill.com/itp/bess-media/audio.json';
+    data = a.loadJSON(url);
+  }
 
-// let viz_2 = function(b) {
-//   b.preload = function() {
-//     song_2 = b.loadSound('/media/LoveForYou.mp3');
-//   }
-//
-//   b.setup = function() {
-//     canvas = b.createCanvas(576, 192);
-//     canvas.parent('song_viz_2');
-//     b.colorMode(b.HSB);
-//     b.angleMode(b.DEGREES);
-//     slider_2 = b.createSlider(0, 1, 0.75, 0.01);
-//     slider_2.parent('controls_2');
-//
-//     playbutton = b.createButton('Play');
-//     playbutton.parent('controls_2');
-//     playbutton.mousePressed(b.playsound);
-//
-//     pausebutton = b.createButton('Pause');
-//     pausebutton.parent('controls_2');
-//     pausebutton.mousePressed(b.pausesound);
-//
-//     b.fft = new p5.FFT(0.3, 4096);
-//     b.fft.setInput(song_2);
-//     song_2.stop();
-//     w = b.width / 300;
-//   }
-//
-//   b.draw = function() {
-//     b.background(40);
-//     song_2.setVolume(slider_2.value())
-//     let spectrum = b.fft.analyze();
-//     b.noStroke();
-//     b.beginShape();
-//     for (let i = 0; i < spectrum.length; i++) {
-//       let amp = spectrum[i];
-//       let y = b.map(amp, 25, 256, b.height, 10);
-//       b.fill(170, i, 255);
-//       b.rect(i * w * 1.5, y + 7.5, w / 0.7, b.height - y);
-//     }
-//     b.endShape();
-//   }
-//
-//   b.playsound = function() {
-//     if (song_2.isPlaying() == false) {
-//       song_2.play();
-//     }
-//   }
-//
-//   b.pausesound = function() {
-//     if (song_2.isPlaying() == true) {
-//       song_2.pause();
-//     }
-//   }
-// };
-// new p5(viz_2, 'song_viz_2');=
+  a.setup = function() {
+    song = a.loadSound(data[1].file, a.loaded, a.failed, a.loading);
+    canvas = a.createCanvas(576, 192);
+    canvas.parent('song_viz');
+    a.colorMode(a.HSB);
+    a.angleMode(a.DEGREES);
+    slider_1 = a.createSlider(0, 1, 0.75, 0.01);
+    slider_1.parent('controls_1');
+
+    playbutton = a.createButton('Play');
+    playbutton.parent('controls_1');
+    playbutton.mousePressed(a.playsound);
+
+    pausebutton = a.createButton('Pause');
+    pausebutton.parent('controls_1');
+    pausebutton.mousePressed(a.pausesound);
+
+    a.fft = new p5.FFT(0.3, 4096);
+    a.fft.setInput(song);
+    song.stop();
+    w = a.width / 300;
+  }
+
+  a.draw = function() {
+    a.background(40);
+    song.setVolume(slider_1.value())
+    let spectrum = a.fft.analyze();
+    a.noStroke();
+    a.beginShape();
+    for (let i = 0; i < spectrum.length; i++) {
+      let amp = spectrum[i];
+      let y = a.map(amp, 25, 256, a.height, 10);
+      a.fill(170, i, 255);
+      a.rect(i * w * 1.5, y + 7.5, w / 0.7, a.height - y);
+    }
+    a.endShape();
+  }
+  a.loaded = function() {
+    let div = a.createDiv('Audio Loaded!');
+    setTimeout(function() {
+      div.hide();
+      loadingP.style.display = "none";
+      }, 3000);
+  }
+
+  a.failed = function() {
+    a.createDiv('Error: Audio Failed to Load')
+  }
+
+  a.loading = function(percent) {
+    pct = Math.round((percent * 100).toFixed(1) / 10) * 10 + "%";
+    loadingP.style.display = "block";
+    document.getElementById("loading").innerHTML += pct;
+  }
+
+  a.playsound = function() {
+    if (song.isPlaying() == false) {
+      song.play();
+    }
+  }
+
+  a.pausesound = function() {
+    if (song.isPlaying() == true) {
+      song.pause();
+    }
+  }
+};
+let viz_3 = function(a) {
+  a.preload = function () {
+    let url = 'https://patrickmcneill.com/itp/bess-media/audio.json';
+    data = a.loadJSON(url);
+  }
+
+  a.setup = function() {
+    song = a.loadSound(data[2].file, a.loaded, a.failed, a.loading);
+    canvas = a.createCanvas(576, 192);
+    canvas.parent('song_viz');
+    a.colorMode(a.HSB);
+    a.angleMode(a.DEGREES);
+    slider_1 = a.createSlider(0, 1, 0.75, 0.01);
+    slider_1.parent('controls_1');
+
+    playbutton = a.createButton('Play');
+    playbutton.parent('controls_1');
+    playbutton.mousePressed(a.playsound);
+
+    pausebutton = a.createButton('Pause');
+    pausebutton.parent('controls_1');
+    pausebutton.mousePressed(a.pausesound);
+
+    a.fft = new p5.FFT(0.3, 4096);
+    a.fft.setInput(song);
+    song.stop();
+    w = a.width / 300;
+  }
+
+  a.draw = function() {
+    a.background(40);
+    song.setVolume(slider_1.value())
+    let spectrum = a.fft.analyze();
+    a.noStroke();
+    a.beginShape();
+    for (let i = 0; i < spectrum.length; i++) {
+      let amp = spectrum[i];
+      let y = a.map(amp, 25, 256, a.height, 10);
+      a.fill(170, i, 255);
+      a.rect(i * w * 1.5, y + 7.5, w / 0.7, a.height - y);
+    }
+    a.endShape();
+  }
+  a.loaded = function() {
+    let div = a.createDiv('Audio Loaded!');
+    setTimeout(function() {
+      div.hide();
+      loadingP.style.display = "none";
+      }, 3000);
+  }
+
+  a.failed = function() {
+    a.createDiv('Error: Audio Failed to Load')
+  }
+
+  a.loading = function(percent) {
+    pct = Math.round((percent * 100).toFixed(1) / 10) * 10 + "%";
+    loadingP.style.display = "block";
+    document.getElementById("loading").innerHTML += pct;
+  }
+
+  a.playsound = function() {
+    if (song.isPlaying() == false) {
+      song.play();
+    }
+  }
+
+  a.pausesound = function() {
+    if (song.isPlaying() == true) {
+      song.pause();
+    }
+  }
+};
